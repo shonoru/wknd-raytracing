26337d92ad7843d609d7388d7a8bd10ec1ecc53f
Andrey Bodoev <shonoru@gmail.com>
2023-09-17 16:50:35 +0800

    feat: add random double functions

--- rtweekend.h
+++ rtweekend.h
@@ -17,6 +17,19 @@ inline double degrees_to_radians(double degrees)
     return degrees * pi / 180.0;
 }
 
+// CUR What's inline?
+// REF https://www.geeksforgeeks.org/inline-functions-cpp/
+inline double random_double()
+{
+    // returns a random real in [0, 1]
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max)
+{
+    return min + (max - min) * random_double();
+}
+
 #include "interval.h"
 #include "ray.h"
 #include "vec3.h"
