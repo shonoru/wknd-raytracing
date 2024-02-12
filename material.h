757e00588dd1cde48dcbf385de9ec7a131479f78
Andrey Bodoev <shonoru@gmail.com>
2024-02-12 18:04:06 +0800

    feat: add hollow

--- material.h
+++ material.h
@@ -79,7 +79,7 @@ public:
         bool cannot_refract = refraction_ratio * sin_theta > 1.0;
         vec3 direction;
 
-        if (cannot_refract)
+        if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double())
         {
             direction = reflect(unit_direction, rec.normal);
         }
@@ -98,6 +98,15 @@ public:
 
 private:
     double ir; // index of refraction
+
+    static double reflectance(double cosine, double ref_idx)
+    {
+        // use schlick's approximation for reflectance
+
+        auto r0 = (1 - ref_idx) / (1 + ref_idx);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * pow((1 - cosine), 5);
+    }
 };
 
 #endif
